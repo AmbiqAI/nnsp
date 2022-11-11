e23a90a24b55e81db93c2b2a007e50c66074e1bb
poyupaulchen <poyupaulchen@gmail.com>
2022-11-11 12:18:50 -0800

    udpate  ../ns-nnsp/module.mk

--- ns-nnsp/module.mk
+++ ns-nnsp/module.mk
@@ -1,9 +1,5 @@
 local_src := $(wildcard $(subdirectory)/src/*.c)
 includes_api += $(subdirectory)/includes-api
-includes_api += extern/CMSIS/CMSIS_5-5.9.0/CMSIS/Core/Include
-includes_api += extern/CMSIS/CMSIS_5-5.9.0/CMSIS/NN/Include
-includes_api += extern/CMSIS/CMSIS_5-5.9.0/CMSIS/DSP/Include
-includes_api += extern/CMSIS/CMSIS_5-5.9.0/CMSIS/DSP/PrivateInclude
 
 local_bin := $(BINDIR)/$(subdirectory)
 bindirs   += $(local_bin)
